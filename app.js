d1d0753ed6f9c29bd5401422119490926c4fd1a0
Kevin Kasozi <Kevinkasozi@gmail.com>
2021-01-23 21:50:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, This is the year for upskilling. Impoving yourself however you can. This is the year for change !!'));
+app.get('/', (req, res) => res.send('Hello World!, This is the year for upskilling. Impoving yourself however you can. This is the year for change. Now you have a real taste of what it takes to become a devops. Kepp revising and practising !!!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
