0bcbede922931b70bdbe3118e49b89bdfcba3eda
Kevin Kasozi <Kevinkasozi@gmail.com>
2021-01-23 20:21:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!, This is the year for upskilling. Impoving yourself however you can. This is the year for change !!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
